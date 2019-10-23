610c311dc995f8b2d3f23921815cd8f1d67fbf92
Jeff <jeff@jeffcamera.com>
2019-10-23 14:06:17 -0400

    fixed capitalization type-o

--- src/DataAccess/Models/Following.cs
+++ src/DataAccess/Models/Following.cs
@@ -4,7 +4,7 @@ namespace api.DataAccess.Models {
 	public class Following {
 		public long Id { get; set; }
 		public long FollowerUserAccountId { get; set; }
-		public long FolloweeuserAccountId { get; set; }
+		public long FolloweeUserAccountId { get; set; }
 		public DateTime DateFollowed { get; set; }
 		public DateTime? DateUnfollowed { get; set; }
 	}
