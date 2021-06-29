4e3eb420ae9684f48eb2a2e2839e7f92fbef9425
Jeff <jeff@jeffcamera.com>
2021-06-29 08:57:59 -0400

    updated conversions analytics report

--- src/DataAccess/Models/ConversionsReportRow.cs
+++ src/DataAccess/Models/ConversionsReportRow.cs
@@ -3,16 +3,12 @@ using System;
 namespace api.DataAccess.Models {
 	public class ConversionsReportRow {
 		public DateTime Week { get; set; }
-		public int VisitCount { get; set; }
+		public int VisitorCount { get; set; }
 		public int SignupCount { get; set; }
 		public double SignupConversion { get; set; }
-		public int ShareCount { get; set; }
-		public double ShareConversion { get; set; }
-		public int ArticleViewCount { get; set; }
-		public double ArticleViewConversion { get; set; }
-		public int ArticleReadCount { get; set; }
-		public double ArticleReadConversion { get; set; }
-		public int PostTweetCount { get; set; }
-		public double PostTweetConversion { get; set; }
+		public int ArticleViewerCount { get; set; }
+		public double ArticleViewerConversion { get; set; }
+		public int ArticleReaderCount { get; set; }
+		public double ArticleReaderConversion { get; set; }
 	}
 }
