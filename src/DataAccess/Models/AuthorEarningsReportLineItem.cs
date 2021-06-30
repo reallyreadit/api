209d49f981337f0fb299cb8be1230f0e2b7d75fe
Jeff <jeff@jeffcamera.com>
2021-06-30 10:11:21 -0400

    added donation info to earnings report line item

--- src/DataAccess/Models/AuthorEarningsReportLineItem.cs
+++ src/DataAccess/Models/AuthorEarningsReportLineItem.cs
@@ -5,6 +5,8 @@ namespace api.DataAccess.Models {
 		public string AuthorSlug { get; set; }
 		public long? UserAccountId { get; set; }
 		public string UserAccountName { get; set; }
+		public long? DonationRecipientId { get; set; }
+		public string DonationRecipientName { get; set; }
 		public int MinutesRead { get; set; }
 		public int AmountEarned { get; set; }
 		public int AmountPaid { get; set; }
