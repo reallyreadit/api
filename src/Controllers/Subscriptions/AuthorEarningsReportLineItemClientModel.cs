971512f69ec97f45a1e03f71a27928b6558cc9d0
Jeff <jeff@jeffcamera.com>
2021-06-30 10:11:53 -0400

    added DonationRecipientName to earnings report

--- src/Controllers/Subscriptions/AuthorEarningsReportLineItemClientModel.cs
+++ src/Controllers/Subscriptions/AuthorEarningsReportLineItemClientModel.cs
@@ -8,12 +8,14 @@ namespace api.Controllers.Subscriptions {
 			AuthorName = lineItem.AuthorName;
 			AuthorSlug = lineItem.AuthorSlug;
 			UserAccountName = lineItem.UserAccountName;
+			DonationRecipientName = lineItem.DonationRecipientName;
 			MinutesRead = lineItem.MinutesRead;
 			AmountEarned = lineItem.AmountEarned;
 		}
 		public string AuthorName { get; }
 		public string AuthorSlug { get; }
 		public string UserAccountName { get; }
+		public string DonationRecipientName { get; }
 		public int MinutesRead { get; }
 		public int AmountEarned { get; }
 	}
